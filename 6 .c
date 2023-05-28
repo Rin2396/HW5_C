4a3dedcc2d8dd2b865bdaa967b9df03e4f69c6cf
Rin2396 <114080477+Rin2396@users.noreply.github.com>
2023-05-28 20:55:55 +0300

    Add files via upload

---
+++ 6 .c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int abs_arr(int arr[], int n) {
+    int counter = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (*(arr + i) < 0) {
+            *(arr + i) = *(arr + i) * (-1); // меняем отрицательные числа на положительные
+            counter++; // и прибавляем к счетчику
+        }
+    }
+
+    return counter;
+}
+
+int main() {
+    int n;
+    scanf ("%d", &n); // вводим размер массива
+    
+    int arr[n];
+
+    for (int i = 0; i < n; i++) { // ввод
+        scanf ("%d", &arr[i]);
+    }
+
+    printf ("%d ", abs_arr(arr, n)); // меняем отрицательные числа на положительные и выводим значение счетчика
+
+    for (int i = 0; i < n; i++) { // выводим измененный массив
+        printf("%d ", arr[i]);
+    }
+}
